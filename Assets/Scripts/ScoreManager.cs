7d1ba8b57683c1b8c0f81c9cdf253c077e938c27
TurtleSSP <simon@thesslin.com>
2024-01-15 10:58:48 +0100

    game

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    int currentScore = 0;
+    int scoreToAdd = 0;
+    int clickAmount = 1;
+
+    void Start()
+    {
+        InvokeRepeating("AddAutoScore", 0, 1);
+    }
+
+    void AddAutoScore()
+    {
+        currentScore = currentScore + scoreToAdd;
+        Debug.Log(currentScore);
+    }
+
+    public void AddScore()
+    {
+        currentScore = currentScore + clickAmount;
+    }
+
+    public void Upgrade1()
+    {
+        scoreToAdd++;
+        Debug.Log(currentScore);
+    }
+}
